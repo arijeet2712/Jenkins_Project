89ee5dc7e34f163938598ffd81b8fe686d927b3c
arijeet <arijeet.saha27@gmail.com>
2021-01-17 05:18:11 +0000

    Add Project Files

---
+++ webapp/src/test/java/com/maven/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
